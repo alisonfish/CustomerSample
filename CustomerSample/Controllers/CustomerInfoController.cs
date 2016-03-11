5ee969d507021d0d4a1b030c2f080b5321526f7f
alison <s250376@hotmail.com>
2016-03-12 03:45:03 +0800

    Week1Homework Finish

---
+++ CustomerSample/Controllers/CustomerInfoController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomerSample.Models;
+using System.Net;
+using System.Data.Entity;
+
+namespace CustomerSample.Controllers
+{
+    public class CustomerInfoController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+
+        public ActionResult CustomerRelatedList()
+        {
+            return View(db.vw_CustomerRelatedData.ToList());
+        }
+
+        // GET: CustomerInfo
+        public ActionResult Index(string keyword)
+        {
+            var data = db.客戶資料.Where(p => false == p.Deleted).AsQueryable();
+
+            if (!String.IsNullOrEmpty(keyword))
+            {
+                data = data.Where(p => p.客戶名稱.Contains(keyword));
+            }
+
+            return View(data.ToList());
+        }
+
+        // GET: CustomerInfo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 customerInfo = db.客戶資料.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        // GET: CustomerInfo/Create
+        public ActionResult Create()
+        {
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱");
+            return View();
+        }
+
+        // POST: CustomerInfo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 customerInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶資料.Add(customerInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customerInfo);
+        }
+
+        // GET: CustomerInfo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 customerInfo = db.客戶資料.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        // POST: CustomerInfo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 customerInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customerInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customerInfo);
+        }
+
+        // GET: CustomerInfo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶資料 customerInfo = db.客戶資料.Find(id);
+            if (customerInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customerInfo);
+        }
+
+        // POST: CustomerInfo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶資料 customerInfo = db.客戶資料.Find(id);
+            customerInfo.Deleted = true;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
